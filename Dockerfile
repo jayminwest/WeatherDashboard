a596dbda7e7ced3ee74af3d62530727e890f99a1
jaymin <jayminwest@gmail.com>
2023-12-18 15:48:39 -0800

    added function to get current weather

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM python:3.11
 WORKDIR /app
 COPY . /app/
+
+# Installing dependencies:
+RUN pip install --no-cache-dir -r requirements.txt
+
 CMD ["python", "WeatherData.py"]
--- WeatherData.py
+++ WeatherData.py
@@ -1,1 +1,29 @@
-print("Weather Data")
+import requests
+from datetime import datetime
+
+"""
+1. Enter location
+2. Get the coords for that location
+3. Call api using that location
+4. Show the current forecast for the next week and show the historical median from years past
+5. Print this out
+6. Add a section for moisture as well
+
+"""
+
+OWM_API_KEY = ''
+
+def get_current_weather(apiKey, cityName):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': cityName, 
+        'appid': apiKey,
+        'units': 'metric'
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    return data
+
+print(get_current_weather(OWM_API_KEY, "Sandpoint"))
